d5141d1a0eb9afaddb4a09876491e733f5d6d609
vaibhav164 <va1641996@gmail.com>
2022-07-27 10:07:38 +0530

    Stipe Payment

--- src/Screens/ChartScreen.tsx
+++ src/Screens/ChartScreen.tsx
@@ -4,23 +4,73 @@ import {TouchableOpacity} from 'react-native-gesture-handler';
 export const ChartScreen = ({route, navigation}) => {
   const {value} = route.params;
   return (
-    <View style={{paddingTop: 40}}>
-      <TouchableOpacity
-        onPress={() => {
-          navigation.goBack();
-          console.log('sdfsdd', value);
+    <View style={{flex: 1}}>
+      <View
+        style={{
+          flex: 0.1,
+          paddingBottom: '4%',
+          justifyContent: 'flex-end',
+          paddingLeft: '4%',
+          alignItems: 'flex-start',
+          height: '100%',
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.goBack();
+            console.log('sdfsdd', value);
+          }}>
+          <Text style={{fontSize: 18, fontWeight: 'bold', color: '#D61C4E'}}>
+            {'<-'}GoBack
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          flex: 0.9,
+          alignItems: 'center',
         }}>
         <View
           style={{
-            width: '25%',
-            marginLeft: 20,
-            marginTop: 20,
+            borderColor: '#0096FF',
+            borderWidth: 2,
+            justifyContent: 'center',
+            paddingTop: '10%',
+            alignItems: 'center',
+            height: '30%',
+            borderRadius: 30,
+            width: '80%',
           }}>
-          <Text style={{fontSize: 18, fontWeight: 'bold', color: 'red'}}>
-            {'<- Go Back'}
+          <Text
+            style={{
+              fontSize: 40,
+              fontWeight: 'bold',
+              position: 'absolute',
+              paddingBottom: '30%',
+            }}>
+            ${value}
           </Text>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#0096FF',
+              width: 150,
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 40,
+            }}>
+            <View>
+              <Text
+                style={{
+                  fontSize: 40,
+                  fontWeight: '700',
+                  fontStyle: 'italic',
+                  color: 'white',
+                }}>
+                Pay
+              </Text>
+            </View>
+          </TouchableOpacity>
         </View>
-      </TouchableOpacity>
+      </View>
     </View>
   );
 };
